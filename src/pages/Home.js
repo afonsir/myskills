cc2ab21b3f15bea42adca85ed97770dc718b3f26
AfonSir <afonsir17@gmail.com>
2021-08-17 21:17:18 -0300

    fix: define skill card key

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,7 +39,7 @@ export function Home() {
 
       {
         mySkills.map(skill => (
-          <SkillCard skill={skill} />
+          <SkillCard key={skill} skill={skill} />
         ))
       }
     </View >
